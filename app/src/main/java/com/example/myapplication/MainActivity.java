5c7dad32ce1c1a500cbc51e6c61e022a0e1e3f48
Hashan9625 <61758181+Hashan9625@users.noreply.github.com>
2021-03-14 20:25:14 +0530

    add python file

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,29 +3,44 @@
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+//import com.chaquo.python.PyObject;
+//import com.chaquo.python.Python;
+//import com.chaquo.python.android.AndroidPlatform;
+
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
-
 import java.util.ArrayList;
+import java.util.Locale;
 
 import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
 
 //    Button chenge ;
+
+
+
+
     pl.droidsonroids.gif.GifImageView animation;
     TextView text;
+    TextView text2;
     int i =1;
-    private static final int RECRESULT = 1;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    public String que ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,62 +48,169 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        chenge = (Button) findViewById(R.id.but);
+
+
         animation = (pl.droidsonroids.gif.GifImageView)findViewById(R.id.ani);
         text = (TextView)findViewById(R.id.tex);
+        text2= (TextView)findViewById(R.id.tex2);
 
 
 //        chenge.getDisplay(no)
 
-//        if (! Python.isStarted()) {
-//            Python.start(new AndroidPlatform(this));
-//        }
-//        //start python
-//
-//        Python py = Python.getInstance();
-//
-//        PyObject pyobj = py.getModule("myPython");
-//
-//        PyObject obj = pyobj.callAttr("main");
-//
-//        text.setText(obj.toString());
+        if (! Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+        //start python
+
+        Python py = Python.getInstance();
+        PyObject pyobj = py.getModule("myPython");
+        PyObject obj = pyobj.callAttr("main");
+
+        text2.setText(obj.toString());
 
 
     }
 
     public void onClick(View view){
-
-//        if(i==1){
-//            animation.setImageResource(R.drawable.giphysad);
-//            i=2;
-//        }
-//        else if(i==2)
-//        {
-//            animation.setImageResource(R.drawable.giphyhapy);
-//            i=1;
-//        }
-
-
-        // speech to text
-        Intent speechIntent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        speechIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,"speech");
-        startActivityForResult(speechIntent,RECRESULT);  //  -------------------   run time error
-
+        promptSpeechInput();
 
     }
 
-    // speech to text
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
-        if(requestCode==RECRESULT && resultCode == RESULT_OK)
-        {
-            ArrayList<String> mach = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            text.setText(mach.get(0).toString());
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Python py = Python.getInstance();
+        PyObject pyobj = py.getModule("myPython");
+        PyObject obj = pyobj.callAttr("main");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    text.setText(result.get(0));
+                    que = result.get(0);
+                    display();
+                    text2.setText(obj.toString());
+                }
+                break;
+            }
 
         }
-
-        super.onActivityResult(requestCode, resultCode, data);
     }
+
+
+
+
+    private void display(){
+
+        if(que.contains("hi"))
+        {
+            animation.setImageResource(R.drawable.giphya);
+        }
+
+        if(que.contains("hello"))
+        {
+            animation.setImageResource(R.drawable.giphyhapy);
+        }
+        if(que.contains("love"))
+        {
+            animation.setImageResource(R.drawable.giphyc);
+        }
+        if(que.contains("like"))
+        {
+            animation.setImageResource(R.drawable.giphyd);
+        }
+        if(que.contains("bad"))
+        {
+            animation.setImageResource(R.drawable.giphysad);
+        }
+        if(que.contains("may") || que.contains("ask"))
+        {
+            animation.setImageResource(R.drawable.may);
+        }
+        if(que.contains("idiot") || que.contains("donkey"))
+        {
+            animation.setImageResource(R.drawable.idiot);
+        }
+        if(que.contains("big") || que.contains("long"))
+        {
+            animation.setImageResource(R.drawable.big);
+        }
+
+        if(que.contains("do you") || que.contains("know"))
+        {
+            animation.setImageResource(R.drawable.doyou);
+        }
+
+        if(que.contains("right") || que.contains("correct"))
+        {
+            animation.setImageResource(R.drawable.right);
+        }
+
+        if(que.contains("crazy") || que.contains("funny"))
+        {
+            animation.setImageResource(R.drawable.crazy);
+        }
+
+        if(que.contains("wanna") || que.contains(" me "))
+        {
+            animation.setImageResource(R.drawable.iwanna);
+        }
+
+        if(que.contains("may") || que.contains("ask"))
+        {
+            animation.setImageResource(R.drawable.may);
+        }
+
+        if(que.contains("are you"))
+        {
+            animation.setImageResource(R.drawable.areyou);
+        }
+
+        if(que.contains("beautiful") || que.contains("pretty"))
+        {
+            animation.setImageResource(R.drawable.beautiful);
+        }
+
+        if(que.contains("how"))
+        {
+            animation.setImageResource(R.drawable.how);
+        }
+
+        if(que.contains("ok"))
+        {
+            animation.setImageResource(R.drawable.ok);
+        }
+
+        if(que.contains("what"))
+        {
+            animation.setImageResource(R.drawable.what);
+        }
+
+    }
+
+
 }
 
